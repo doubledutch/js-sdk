1a202b5477dbdc3a90e548592c683bc3443000ea
Adam Liechty <adam@doubledutch.me>
2014-01-16 15:50:37 -0800

    Get current event sample.

--- samples/basic/Demo.js
+++ samples/basic/Demo.js
@@ -7,11 +7,20 @@ $(function () {
 
     // Get the current user in the app.
     DD.Events.getCurrentUserAsync(function (user) {
+      $("#user .image").attr("src", user.ImageUrl);
       $("#user-firstName").text(user.FirstName);
       $("#user-lastName").text(user.LastName);
       $("#user-company").text(user.Company);
       $("#user-title").text(user.Title);
-      $("#user-image").attr("src", user.ImageUrl);
+    });
+
+    // Get the current event in the app.
+    DD.Events.getCurrentEventAsync(function (event) {
+      $("#event .image").attr("src", event.Icon);
+      $("#event-name").text(event.Name);
+      $("#event-desc").text(event.Description);
+      $("#event-start").text(new Date(event.StartDate).toDateString());
+      $("#event-end").text(new Date(event.EndDate).toDateString());
     });
 
     //DD.Events.getSignedAPIAsync("users/?so=Score&sd=Descending&count=5", "", function (signedUrl) {
